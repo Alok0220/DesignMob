1aeeae539295bd85f6a849effb54a4e98b87f323
Alok <dethealok07@gmail.com>
2021-09-23 17:08:10 +0530

    recent

--- app/src/main/java/com/example/chap2/MainActivity.java
+++ app/src/main/java/com/example/chap2/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.chap2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.TextureView;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+
+    private static int SPLASH_SCREEN = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animate);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animate);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.shopAppId);
+        slogan = findViewById(R.id.buyOnlineId);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity( intent );
+                finish(); //imp
+            }
+        }, SPLASH_SCREEN);
     }
 }
